f03a9c6823818f029156be7ceceb8cc8686353ba
Manhunter7 <thomas.piquet7@gmail.com>
2022-02-02 13:53:52 +0100

    2//2/21 best of v2

---
+++ src/lsg/consumables/MenuBestOfV2.java
@@ -0,0 +1,29 @@
+package lsg.consumables;
+
+import java.util.HashSet;
+
+import lsg.consumables.drinks.Wine;
+import lsg.consumables.food.Hamburger;
+import lsg.consumables.drinks.Coffee;
+import lsg.consumables.drinks.Whisky;
+import lsg.consumables.food.Americain;
+
+public class MenuBestOfV2 extends HashSet<Consumable> {
+
+    public MenuBestOfV2(){
+        this.add(new Wine());
+        this.add(new Hamburger());
+        this.add(new Coffee());
+        this.add(new Whisky());
+        this.add(new Americain());
+    }
+
+    @Override
+    public String toString(){
+        String rtn = "MenuBestOfV2 :\n";
+        for (Consumable c: this){
+            rtn += c + " : " + c.getName() + " [ " + c.getCapacity() + c.getStat() + "points]\n";
+        }
+        return rtn;
+    }
+}
